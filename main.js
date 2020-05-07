5cd42352eecd45d94ee05406451b46839f9b0079
Jon Gadsden <jon.gadsden@forgerock.com>
2020-05-07 19:11:06 +0100

    reorganised the CLI handling

--- main.js
+++ main.js
@@ -4,19 +4,26 @@ const path = require('path');
 const electron = require('electron');
 const app = electron.app;
 const setupEvents = require('./config/squirrel');
+var command;
 if (setupEvents.handleSquirrelEvent(app)) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
         return;
 }
+
+// use yargs to provide a command line interface
+// can call it using './electron . --help' after 'ln -sf node_modules/electron/cli.js electron'
 const argv = require('yargs')
   .usage('Usage: $0 <command> [options]')
   .command({
     command: 'report <json> <pdf>',
-    desc: 'Export the JSON threat model to a PDF report',
+    aliases: ['r', 'rep'],
+    desc: 'Export a JSON threat model as a PDF report',
     handler: (argv) => {
       if (argv.verbose) {
         console.log(`creating report ${argv.pdf} from ${argv.json}`)
       }
+      command = 'report';
+      app.quit();
     }
   })
   .demandCommand(0, 0, '', 'Command not recognised')
@@ -27,16 +34,29 @@ const argv = require('yargs')
       type: 'boolean'
     }
   })
+  .help()
+  .wrap(80)
   .argv;
 
 // prevent window being garbage collected
 let mainWindow;
 
-function checkIfCalledViaCLI(args) {
-  if(args && args.length > 2){
-    return true;
+function checkIfValidCommand() {
+  if (command != 'report') {
+    return false;
   }
-  return false;
+
+  return true;
+}
+
+function doCommand() {
+
+  mainWindow.hide();
+
+  if (argv.verbose) {
+    console.log('CLI called with command', command, 'and', process.argv.length, 'arguments');
+  }
+
 }
 
 function onClosed() {
@@ -45,9 +65,30 @@ function onClosed() {
   mainWindow = null;
 }
 
+function createCLI() {
+  var window = new electron.BrowserWindow({ show: false, width: 0, height: 0});
+
+  if (argv.verbose) {
+    console.log('Calling Threat Dragon with CLI interface');
+  }
+
+  window.loadURL(`file://${__dirname}/index.html`);
+  window.on('closed', onClosed);
+  window.webContents.on('new-window', function (e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+
+  return window;
+}
+
 function createMainWindow() {
 
   const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+
+  if (argv.verbose) {
+    console.log('Calling Threat Dragon with user interface');
+  }
 
   var window = new electron.BrowserWindow({
     title: "OWASP Threat Dragon",
@@ -74,28 +115,25 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (!mainWindow) {
+  let isCliCommand = checkIfValidCommand();
+
+  if (!mainWindow && !isCliCommand) {
     mainWindow = createMainWindow();
   }
 });
 
 app.on('ready', () => {
-  let isCalledViaCLI = checkIfCalledViaCLI(process.argv);
+  let isCliCommand = checkIfValidCommand();
 
-  if (isCalledViaCLI) {
-    mainWindow = new electron.BrowserWindow({ show: false, width: 0, height: 0});
-    if (argv.verbose) {
-      console.log('called with',process.argv.length, 'arguments');
-    }
-    app.quit();
-  } else {
+  if (!isCliCommand) {
     mainWindow = createMainWindow();
+  } else {
+    mainWindow = createCLI();
+    doCommand();
   }
 
   mainWindow.once('ready-to-show', () => {
-    if (isCalledViaCLI) {
-      mainWindow.hide();
-    } else {
+    if (!isCliCommand) {
       mainWindow.show();
       mainWindow.maximize();
     }
